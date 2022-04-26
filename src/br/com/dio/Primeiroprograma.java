7b7e81cdbc05e347c9eee044ac5c80da1ed002b3
Monster243108 <evangelistanovo@gmail.com>
2022-04-26 14:41:45 -0300

    First commit

--- src/br/com/dio/Primeiroprograma.java
+++ src/br/com/dio/Primeiroprograma.java
@@ -1,4 +1,4 @@
-package br.com.dio;
+package     br.com.dio;
 
 import br.com.dio.model.Gato;
 
@@ -7,9 +7,55 @@ public class Primeiroprograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro);
+
       /*  int a = 5;
         int b = 3;
         System.out.println("Hello Monster! " + (a+b));*/
     }
 }
+class Livro {
+    private String nome;
+    private String numPaginas;
+
+    public Livro(String nome, String numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public Livro(String nome, int i) {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(String numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas='" + numPaginas + '\'' +
+                '}';
+    }
+}
+
+
+
+
+
+
+
 
